068d2778f61d08e4bd2c53c5bd701eda209212bf
YujiHamada <uziu2hahaha@gmail.com>
2016-01-28 23:17:33 +0900

    モデルのバリデーション追加しました。 エラーメッセージの出し方わかんなくてこまってます

---
+++ app/Model/Thread.php
@@ -0,0 +1,11 @@
+
+<?php  
+class Thread extends AppModel {
+	 public $validate = array(
+	 		'comment' =>array(
+	 			'rule' => array( 'maxLength', 200),
+                'message' => '投稿内容は200文字以下で入力して下さい',
+            		)
+			 	);
+	}
+?>
