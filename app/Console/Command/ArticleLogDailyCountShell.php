fae68f1e64d6fc8aabf4fc9d2c688f37aab2a4a6
yamatysan <autumnnavigation000@gmail.com>
2016-09-13 19:35:13 +0900

    日次集計（記事へのアクセス数）からメール送信。（DBに保存はまだ）

---
+++ app/Console/Command/ArticleLogDailyCountShell.php
@@ -0,0 +1,64 @@
+<?php
+App::uses('CakeEmail', 'Network/Email');
+
+//記事の日別アクセス数
+class ArticleLogDailyCountShell extends AppShell {
+
+    public $uses = array('Race', 'Article', 'ArticleLog');
+
+    public function getOptionParser() {
+        $parser = parent::getOptionParser();
+
+        $parser->addOption(
+            'day', array(
+                'help' => 'day [yyyy-mm-dd]',
+                'default' => date('Y-m-d', strtotime('-1 day', time())),
+            )
+        );
+
+        return $parser;
+    }
+
+    public function main() {
+    	$msg = "start shell";
+    	$this->__log($msg);
+
+        $target_date = $this->params['day'];
+        $start = $this->params['day'].' 00:00:00';
+        $end   = $this->params['day'].' 23:59:59';
+
+        //集計する
+        $this->ArticleLog->virtualFields['cnt'] = 0;//これを追加
+        $options = array(
+            'fields' => array('article_id','count(*) as ArticleLog__cnt'),
+            'conditions' => array(
+                'created between ? and ?' => array($start, $end),
+            ),
+            'group' => array('article_id')
+        );
+        $logs = $this->ArticleLog->find('all',$options);
+        if(empty($logs)){
+            $this->__log("finish ok!");
+        }
+
+        foreach($logs as $key=>&$data){
+            //Articleを取得
+            $article = $this->Article->findById($data['ArticleLog']['article_id']);
+            $data['ArticleLog']['name'] = $article['Article']['title'];
+        }
+
+
+        //メール送信
+        $email = new CakeEmail('smtp'); 
+        $email->to('info@sanrentan-box.com');
+        $email->subject( '['.$target_date.'] 記事閲覧数');
+        $email->emailFormat('text');
+        $email->template('article_daily_log');
+        $email->viewVars(compact('logs', 'target_date'));
+        $email->send();
+
+    	$this->__log("finish ok!");
+
+    }
+
+}
